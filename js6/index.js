2a55d49d87aa746d976cee2f7c665370521acf2b
Yoseph Adane <yosephadane63@Gmail.com>
2024-10-11 15:15:19 +0300

    Add files via upload

---
+++ js6/index.js
@@ -0,0 +1,9 @@
+
+let add = (a,b) => a + b;
+console.log(add(3,5))
+
+let multiply = (a,b) => a * b;
+console.log(multiply(31,55));
+
+let great = (name) => "Hello "  + name;
+console.log( great("Joseph"));
